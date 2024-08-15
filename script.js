fc4cf3b09951f13b24e0db199a9ffe06ee96ed5e
Patrik Aparecido Bueno <106555499+patrikbueno15@users.noreply.github.com>
2024-08-15 11:32:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const caixaPrincipal = document.querySelector(".caixa- principal");
+
     const caixaPerguntas = document.querySelector(".caixa-perguntas");
     
     const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -9,18 +10,33 @@ const caixaPrincipal = document.querySelector(".caixa- principal");
     
     const perguntas = [
     {
-    enunciado: "Ensira o anunciado da Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
+    enunciado: "pergunta1",
+    alternativa:["alternativa1", "alternativa2"],
     },
     {
-    enunciado: "Ensira o anunciadoda Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-    let atual = 0;
-    let perguntaAtual;
-    function mostraPergunta() 
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent =perguntaAtual.enunciado;{
-    }
-    mostraPergunta();
+    enunciado: "pergunta2",
+    alternativa: ["alternativa1", "alternativa2"],   
     },
     ];
+    let atual = 0;
+    let perguntaAtual;
+    function mostraPergunta(){ 
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent =perguntaAtual.enunciado;
+    mostraPergunta();
+    mostraAlternativas();
+    }
+    function mostraAlternativas() {
+         for (const alternativa of perguntaAtual.alternativas) {
+              const botaoalternativa=alternativa;
+botaoalternativa.textContent=alternativa;
+caixaAlternativas.appendChild(botaoalternativa);
+       const caixaPerguntas = Document.querySelector}
+(".caixa-perguntas");}
+{     
+enunciado: "Assim que saiu da escola, você sedepara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter.Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o seu primeiro pensamento?",
+    alternativas: [
+        "Isso é assustadoor!",
+        "Isso é maravilhoso!"
+    ]
+    }
