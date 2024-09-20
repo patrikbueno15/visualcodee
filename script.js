71d0cc77d06f4d9edb685ffc38604cd3f1ce0b1f
Patrik Aparecido Bueno <106555499+patrikbueno15@users.noreply.github.com>
2024-09-20 11:31:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,100 @@
-const caixaPrincipal = document.querySelector(".caixa- principal");
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+    
     const caixaPerguntas = document.querySelector(".caixa-perguntas");
     
     const caixaAlternativas = document.querySelector(".caixa-alternativas");
     
     const caixaResultado = document.querySelector(".caixa-resultado");
     
-    const textoResultado = document.querySelector(",texto-resultado");
+    const textoResultado = document.querySelector(".texto-resultado");
     
     const perguntas = [
-    {
-    enunciado: "pergunta1",
-    alternativa:["alternativa1", "alternativa2"],
-    },
-    {
-    enunciado: "pergunta2",
-    alternativa: ["alternativa1", "alternativa2"],   
-    },
+        
+        {
+                enunciado:  "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o seu primeiro pensamento?",   
+                alternativas: [
+                {
+                texto: "Isso é assustador!",
+                afirmação: [
+                    "No início, ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou assustador pensar na velocidade com que a tecnologia está avançando."
+                ]
+                },
+                {
+                texto: "Isso é maravilhoso!",
+                afirmação: [
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                ]
+                }
+                ]
+        },  
+        {
+                enunciado: "Insira o enunciado da Pergunta 2",
+                alternativas: [
+                {
+                texto: "Alternativa 1",
+                afirmação: "Afirmação da alternativa 1"
+                },
+                {
+                texto: "Alternativa 2",
+                afirmação: "Afirmação da alternativa 2"
+                }
+                ]
+        },
+        {
+                enunciado: "Insira o enunciado da Pergunta 3",
+                alternativas: [
+                {
+                texto: "Alternativa 1",
+                afirmação: "Afirmação da alternativa 1"
+                },
+                {
+                texto: "Alternativa 2",
+                afirmação: "Afirmação da alternativa 2"
+                }
+                ]
+                }
     ];
+
     let atual = 0;
     let perguntaAtual;
-    function mostraPergunta(){ 
+    let historiaFinal = "";
+
+    function mostraPergunta() {
+    if (atual >= perguntas.length) {
+            mostraResultado();
+            return;
+            }
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent =perguntaAtual.enunciado;
-    mostraPergunta();
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
     }
+
     function mostraAlternativas() {
-         for (const alternativa of perguntaAtual.alternativas) {
-              const botaoalternativa=alternativa;
-botaoalternativa.textContent=alternativa;
-caixaAlternativas.appendChild(botaoalternativa);
-       const caixaPerguntas = Document.querySelector}
-(".caixa-perguntas");}
-{     
-enunciado: "Assim que saiu da escola, você sedepara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter.Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o seu primeiro pensamento?",
-    alternativas: [
-        "Isso é assustadoor!",
-        "Isso é maravilhoso!"
-    ]
+        for (const alternativa of perguntaAtual.alternativas) {
+            const botaoAlternativas = document.createElement("button");
+            botaoAlternativas.textContent = alternativa.texto;
+            botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+            caixaAlternativas.appendChild(botaoAlternativas);
+            }
+        }
+    
+    function respostaSelecionada(opcaoSelecionada) {
+        const afirmacoes = (opcaoSelecionada.afirmação);
+        historiaFinal = afirmacoes + " ";
+        atual++;
+        mostraPergunta();
     }
+
+    function mostraResultado() {
+        function aleatorio (lista){
+        const posicao = Math.random()*lista.length;
+        return lista[posicao];
+        }
+        caixaPerguntas.textContent = "Em 2049...";
+        textoResultado.textContent = historiaFinal;
+        caixaAlternativas.textContent = "";
+    }
+     mostraPergunta();
